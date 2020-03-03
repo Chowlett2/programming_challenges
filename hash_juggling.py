275a34fe5fbdbf15a1d4923ef586608d4a83aebf
Chowlett2 <55161121+Chowlett2@users.noreply.github.com>
2020-03-02 23:59:41 -0500

    Add files via upload

---
+++ hash_juggling.py
@@ -0,0 +1,21 @@
+'''Create a function that:
+
+Creates a sha256 hash from a list of strings.
+Sorts the hash, alphas first digits second.
+Returns a new sha256 hash from the sorted string.'''
+
+import hashlib
+
+def hash_ops(lst):
+    hash1 = ''
+    for i in lst:
+        hash1 += i
+    hash1 = hashlib.sha256(hash1.encode()).hexdigest()
+    hashsort = ''
+    for i in hash1:
+        if i.isalpha():
+            hashsort += i
+    for i in hash1:
+        if not i.isalpha():
+            hashsort += i
+    return hashlib.sha256(hashsort.encode()).hexdigest()
