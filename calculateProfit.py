885520d85d9a0643925e67d783a1ea4bfd70bf44
Conor Howlett <55161121+Chowlett2@users.noreply.github.com>
2020-03-07 21:47:37 -0500

    Add files via upload

---
+++ calculateProfit.py
@@ -0,0 +1,8 @@
+'''You work for a manufacturer, and have been asked to calculate the total 
+profit made on the sales of a product. You are given a dictionary containing 
+the cost price per unit (in dollars), sell price per unit (in dollars), 
+and the starting inventory. Return the total profit made, rounded to the 
+nearest dollar. Assume all of the inventory has been sold.'''
+
+def profit(info):
+	return round((info["inventory"])*(info["sell_price"] - info["cost_price"]))
